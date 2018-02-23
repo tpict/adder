6e5f52b2aa4c2307325da06d7780c3bc3ae52fed
Tom Picton <tom@tompicton.co.uk>
2018-02-23 23:24:36 +0000

    Initial commit

---
+++ snake.hs
@@ -0,0 +1,48 @@
+import qualified System.Console.ANSI as ANSI
+import           Control.Concurrent
+import           System.IO
+
+move :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+move (dirY, dirX) (x:xs) = init $ (dirY + fst x, dirX + snd x):x:xs
+
+modStr :: (Int, Int) -> [[Char]] -> [[Char]]
+modStr point str = let (beforeLines, line:afterLines) = splitAt (fst point) str
+                       (beforeChars, _:afterChars) = splitAt (snd point) line
+                       newLine = beforeChars ++ ['x'] ++ afterChars
+  in beforeLines ++ [newLine] ++ afterLines
+
+toStr :: [(Int, Int)] -> [[Char]]
+toStr xs = foldl (\acc x -> modStr x acc) (replicate 10 (replicate 20 '.')) xs
+
+charToDir :: Char -> (Int, Int)
+charToDir c
+  | c == 'w' = (-1, 0)
+  | c == 'a' = (0, -1)
+  | c == 's' = (1, 0)
+  | c == 'd' = (0, 1)
+
+movePlease :: Char -> [(Int, Int)] -> [(Int, Int)]
+movePlease c xs = move (charToDir c) xs
+
+snake = ([(0, 2), (0, 1), (0, 0)], Nothing)
+
+main = do
+    c <- newEmptyMVar
+    putMVar c snake
+    -- thingy <- newEmptyMVar
+
+    hSetBuffering stdin NoBuffering
+    hSetEcho stdin False
+    putStrLn $ unlines $ toStr $ fst snake
+    forkIO $ myinput c
+    wait c
+    where wait c = do
+            -- auto move goes here
+            threadDelay 1000000 >> wait c
+          myinput c = do
+            a <- getChar
+            state <- takeMVar c
+            ANSI.cursorUp $ (length $ toStr $ fst state) + 1
+            putStrLn $ unlines $ toStr $ movePlease a $ fst state
+            putMVar c (movePlease a $ fst state, Just a)
+            myinput c
