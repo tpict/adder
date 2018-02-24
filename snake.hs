5f9bb60d76f539eb1f438332f60f4b3b2247f9a7
Tom Picton <tom@tompicton.co.uk>
2018-02-24 10:58:51 +0000

    Implement point data type

--- snake.hs
+++ snake.hs
@@ -2,37 +2,44 @@ import qualified System.Console.ANSI as ANSI
 import           Control.Concurrent
 import           System.IO
 
-move :: Maybe (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
-move Nothing xs = xs
-move (Just (dirY, dirX)) (x:xs) = init $ (dirY + fst x, dirX + snd x):x:xs
+data Point = Point Int Int deriving (Show)
 
-modStr :: (Int, Int) -> [[Char]] -> [[Char]]
-modStr point str = let (beforeLines, line:afterLines) = splitAt (fst point) str
-                       (beforeChars, _:afterChars) = splitAt (snd point) line
+(Point a b) |+| (Point c d) = Point (a + c) (b + d)
+infixl 6 |+|
+
+getY (Point a b) = a
+getX (Point a b) = b
+
+
+move :: Maybe Point -> [Point] -> [Point]
+move Nothing xs = xs
+move (Just dir) (x:xs) = init $ (dir |+| x):x:xs
+
+modStr :: Point -> [[Char]] -> [[Char]]
+modStr point str = let (beforeLines, line:afterLines) = splitAt (getY point) str
+                       (beforeChars, _:afterChars) = splitAt (getX point) line
                        newLine = beforeChars ++ ['x'] ++ afterChars
   in beforeLines ++ [newLine] ++ afterLines
 
-toStr :: [(Int, Int)] -> [[Char]]
+toStr :: [Point] -> [[Char]]
 toStr xs = foldl (\acc x -> modStr x acc) (replicate 10 (replicate 20 '.')) xs
 
-charToDir :: Char -> Maybe (Int, Int)
+charToDir :: Char -> Maybe Point
 charToDir c
-  | c == 'w' = Just (-1, 0)
-  | c == 'a' = Just (0, -1)
-  | c == 's' = Just (1, 0)
-  | c == 'd' = Just (0, 1)
+  | c == 'w' = Just (Point (-1) 0)
+  | c == 'a' = Just (Point 0 (-1))
+  | c == 's' = Just (Point 1 0)
+  | c == 'd' = Just (Point 0 1)
   | otherwise = Nothing
 
-movePlease :: Char -> [(Int, Int)] -> [(Int, Int)]
+movePlease :: Char -> [Point] -> [Point]
 movePlease c xs = move (charToDir c) xs
 
-snake = ([(0, 2), (0, 1), (0, 0)], Nothing)
+snake = ([Point 0 x | x <- reverse [0..2]], Nothing)
 
 main = do
     c <- newEmptyMVar
     putMVar c snake
-    -- thingy <- newEmptyMVar
-
     hSetBuffering stdin NoBuffering
     hSetEcho stdin False
     putStrLn $ unlines $ toStr $ fst snake
@@ -46,5 +53,5 @@ main = do
             state <- takeMVar c
             ANSI.cursorUp $ (length $ toStr $ fst state) + 1
             putStrLn $ unlines $ toStr $ movePlease a $ fst state
-            putMVar c (movePlease a $ fst state, Just a)
+            putMVar c (movePlease a (fst state), Just a)
             myinput c
