d51818c10400a3721e1868dc86d80fd98536c47e
Tom Picton <tom@tompicton.co.uk>
2018-02-24 11:55:07 +0000

    Remove map dimensions from GameState

--- snake.hs
+++ snake.hs
@@ -2,6 +2,10 @@ import qualified System.Console.ANSI as ANSI
 import           Control.Concurrent
 import           System.IO
 
+yMax = 10
+xMax = 20
+emptyMap =  replicate yMax $ replicate xMax '.'
+
 data Point = Point Int Int deriving (Show)
 
 (Point a b) |+| (Point c d) = Point (a + c) (b + d)
@@ -10,23 +14,22 @@ infixl 6 |+|
 getY (Point a b) = a
 getX (Point a b) = b
 
-data GameState = GameState {snake :: [Point], yMax :: Int, xMax :: Int, dir :: Maybe Point} deriving (Show)
+data GameState = GameState {snake :: Maybe [Point], dir :: Maybe Point} deriving (Show)
 
-move :: Maybe Point -> [Point] -> [Point]
+move :: Maybe Point -> Maybe [Point] -> Maybe [Point]
 move Nothing xs = xs
-move (Just dir) (x:xs) = init $ (dir |+| x):x:xs
+move _ Nothing = Nothing
+move (Just dir) (Just (x:xs)) = Just $ init $ (dir |+| x):x:xs
 
-modStr :: Point -> [[Char]] -> [[Char]]
+modStr :: Point -> [String] -> [String]
 modStr point str = let (beforeLines, line:afterLines) = splitAt (getY point) str
                        (beforeChars, _:afterChars) = splitAt (getX point) line
                        newLine = beforeChars ++ ['x'] ++ afterChars
   in beforeLines ++ [newLine] ++ afterLines
 
-emptyMap :: GameState -> [String]
-emptyMap state = replicate (yMax state) $ replicate (xMax state) '.'
-
-toStr :: GameState -> [[Char]]
-toStr state = foldl (\acc x -> modStr x acc) (emptyMap state) $ snake state
+toStr :: GameState -> [String]
+toStr (GameState Nothing _) = ["Game over!"]
+toStr state@(GameState (Just snake) _) = foldl (\acc x -> modStr x acc) emptyMap $ snake
 
 charToDir :: Char -> Maybe Point
 charToDir c
@@ -40,15 +43,11 @@ movePlease :: Char -> GameState -> GameState
 movePlease c state = let newDir = charToDir c
   in GameState {
     snake = move newDir $ snake state,
-    yMax = yMax state,
-    xMax = xMax state,
     dir = newDir
   }
 
 snakey = GameState {
-  snake = [Point 0 x | x <- reverse [0..2]],
-  yMax = 10,
-  xMax = 20,
+  snake = Just [Point 0 x | x <- reverse [0..2]],
   dir = Nothing
 }
 
@@ -66,7 +65,7 @@ main = do
         myinput c = do
           a <- getChar
           state <- takeMVar c
-          ANSI.cursorUp $ (length $ toStr state) + 1
+          ANSI.cursorUp $ yMax + 1
           putStrLn $ unlines $ toStr $ movePlease a state
           putMVar c (movePlease a state)
           myinput c
